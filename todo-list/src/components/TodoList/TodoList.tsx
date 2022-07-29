853a6d442b99bd9140cac7909b4ebd836afd92a1
”Ekaterina <katya.tereshenko.1996@mail.ru>
2022-07-29 13:33:15 +0300

    feat: create Button

--- todo-list/src/components/TodoList/TodoList.tsx
+++ todo-list/src/components/TodoList/TodoList.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoItem from 'components/TodoItem';
 import Input from 'components/Input';
+import Button from 'components/Button';
 
 const TodoList = () => {
   return (
     <div data-testid="todo-container">
       <Input />
+      <Button text="Add task" />
       <TodoItem />
     </div>
   );
